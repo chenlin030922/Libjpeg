8bc025f86673ebcdb1a56d418942b5738001ae05
林晨 <linchen@sogou-inc.com>
2019-03-11 19:11:18 +0800

    添加所有架构的so包

--- app/src/main/java/com/lin/libjpeg/MainActivity.java
+++ app/src/main/java/com/lin/libjpeg/MainActivity.java
@@ -7,6 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
@@ -23,13 +29,34 @@ public class MainActivity extends AppCompatActivity {
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI("da"));
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.youzi);
-        String result = getSaveLocation();
-        nativeCompressBitmap(bitmap, 0,result );
+        String result = getSaveLocation() + "/compress1.png";
+        nativeCompressBitmap(bitmap, 0, result);
+        compressByDefault(bitmap);
     }
 
-    private String getSaveLocation(){
-        return Environment.getExternalStorageDirectory() + "/tttes.png";
+    private void compressByDefault(Bitmap bitmap) {
+        File file = new File(getSaveLocation() + "/compress2.png");
+        if (file.exists()) {
+            try {
+                file.delete();
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            OutputStream stream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 0, stream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
+
+    private String getSaveLocation() {
+        return Environment.getExternalStorageDirectory().getAbsolutePath();
+    }
+
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
